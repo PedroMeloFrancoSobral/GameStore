ab93345d72b8699815e785ede0d69f36aff7b56e
Pedro Sobral <pedrosobral2009@gmail.com>
2023-08-07 15:59:17 -0300

    Create spring-boot project with maven

---
+++ gamestore-spring/src/main/java/com/pedrosobral/gamestorespring/GamestoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pedrosobral.gamestorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestoreSpringApplication.class, args);
+	}
+
+}
---
+++ gamestore-spring/src/test/java/com/pedrosobral/gamestorespring/GamestoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosobral.gamestorespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
